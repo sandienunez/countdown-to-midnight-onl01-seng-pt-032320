7d2bae5ce82e726253007336ba200cdfaac384d1
Sandie Nunez <snunez@vt.edu>
2020-03-31 05:14:00 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -11,4 +11,26 @@ def countdown_with_sleep(int)
   end
     "HAPPY NEW YEAR!"
 end
+def countdown(num)
 
+while num > 0
+
+  puts "#{num} SECOND(S)!"
+  num -= 1
+
+end
+  "HAPPY NEW YEAR!"
+end
+
+
+def countdown_with_sleep(num)
+
+while num > 0
+
+  puts "#{num} SECOND(S)!"
+  sleep(1)
+  num -= 1
+
+end
+  "HAPPY NEW YEAR!"
+end	end
